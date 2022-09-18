e5806917145badec25934127fb29840bfa56d1d1
Makarova Ekaterina <makarova.ekaterina-1410@yandex.ru>
2022-09-18 20:26:09 +0300

    added info about task1

---
+++ task1/Program.cs
@@ -0,0 +1,28 @@
+﻿// Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь
+
+Console.WriteLine($"Введите числа: ");
+int x = Convert.ToInt32(Console.ReadLine());
+int[] arrayNumbers = new int[x];
+
+void InputNumbers(int x){
+for (int i = 0; i < x; i++)
+  {
+    Console.WriteLine($"Введите {i+1} числа: ");
+    arrayNumbers[i] = Convert.ToInt32(Console.ReadLine());
+  }
+}
+
+
+int Comparison(int[] arrayNumbers)
+{
+  int count = 0;
+  for (int i = 0; i < arrayNumbers.Length; i++)
+  {
+    if(arrayNumbers[i] > 0 ) count += 1; 
+  }
+  return count;
+}
+
+InputNumbers(x);
+
+Console.WriteLine($"Введено чисел больше 0: {Comparison(arrayNumbers)} ");
